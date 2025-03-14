43da1a2833ab2bb2215c51bcbcd90add1ce1a268
Jens Sommerfeld <77424985+jsommerfeld1980@users.noreply.github.com>
2025-03-14 15:01:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
 import requests
+from snowflake.snowpark.functions import col
+
 
 #helpful_links = [
     #"https://docs.streamlit.io",
